01762fc198f48a0000aff9efacbfd55c8959dc1f
Germen Wong <1953792796@qq.com>
2024-02-22 11:52:52 +0800

    引入外部属性文件

---
+++ spring6-ioc-xml/src/main/java/com/hgm/springiocxml/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.hgm.springiocxml.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestJdbc {
+
+    @Test
+    public void demo1() {
+        // 原始方法（手动）
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    }
+
+    @Test
+    public void demo2() {
+        // 使用Spring方法注入（自动）
+        ApplicationContext context=new ClassPathXmlApplicationContext("bean-jdbc.xml");
+        DruidDataSource dataSource = context.getBean(DruidDataSource.class);
+        System.out.println(dataSource.getUrl());
+    }
+}
